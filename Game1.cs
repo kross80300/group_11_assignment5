a3fdba62f4b90b543b15dabd8214261851413fd3
kross80300 <kross80300@gmail.com>
2025-10-16 13:09:48 -0500

    Added Spaceship Class

--- Game1.cs
+++ Game1.cs
@@ -8,6 +8,10 @@ public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+    
+    
+    private Spaceship _spaceship;
+    private Model _spaceshipModel;
 
     public Game1()
     {
@@ -19,6 +23,7 @@ public class Game1 : Game
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
+        
 
         base.Initialize();
     }
@@ -28,6 +33,14 @@ public class Game1 : Game
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
         // TODO: use this.Content to load your game content here
+        _spaceshipModel = Content.Load<Model>("meshes/shuttle");
+
+        Vector3 start = new Vector3(-300f, 0f, 0f);
+        Vector3 end = new Vector3(300f, 0f, 0f);
+        float travelTime = 5f;
+        float pauseTime = 3f;
+        
+        _spaceship = new Spaceship(_spaceshipModel, start, end, travelTime, pauseTime);
     }
 
     protected override void Update(GameTime gameTime)
@@ -37,15 +50,27 @@ public class Game1 : Game
             Exit();
 
         // TODO: Add your update logic here
+        _spaceship.Update(gameTime);
 
         base.Update(gameTime);
     }
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
+        GraphicsDevice.Clear(Color.Black);
 
         // TODO: Add your drawing code here
+        Matrix view = Matrix.CreateLookAt(new Vector3(0, 50, 500),
+            Vector3.Zero,
+            Vector3.Up);
+        
+        Matrix projection = Matrix.CreatePerspectiveFieldOfView(
+            MathHelper.ToRadians(45f),
+            GraphicsDevice.Viewport.AspectRatio,
+            1f,
+            10000f);
+        
+        _spaceship.Draw(view, projection);
 
         base.Draw(gameTime);
     }
---
+++ Spaceship.cs
@@ -0,0 +1,110 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace group_11_assignment5;
+
+public class Spaceship
+{
+    
+    private Model _mesh;
+    private Vector3 _startPoint;
+    private Vector3 _endPoint;
+    private float _totalTime;
+    private float _currentTime;
+    private bool _movingForward;
+
+    private float _pauseTime;
+    private float _currentPause;
+    private bool _isPaused;
+
+    private float _tiltAngle;
+    
+    public Spaceship(Model mesh, Vector3 _startPoint, Vector3 _endPoint, float _totalTime, float _pauseTime)
+    {
+        
+        this._mesh = mesh;
+        this._startPoint = _startPoint;
+        this._endPoint = _endPoint;
+        this._totalTime = _totalTime;
+        this._currentTime = 0f;
+        this._movingForward = true;
+        this._pauseTime = _pauseTime;
+        this._currentPause = 0f;
+        this._isPaused = false;
+        this._tiltAngle = 0f;
+    }
+
+    public void Update(GameTime gameTime)
+    {
+        float elapsedTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+        if (_isPaused)
+        {
+            _currentPause += elapsedTime;
+            if (_currentPause >= _pauseTime)
+            {
+                _currentPause = 0f;
+                _isPaused = false;
+                _movingForward = !_movingForward;
+            }
+
+            return;
+        }
+
+        if (_movingForward)
+        {
+            _currentTime += elapsedTime;
+            _tiltAngle = MathHelper.Lerp(0f, 3.0f, _currentTime/_totalTime);
+        }
+        else
+        {
+            _currentTime -= elapsedTime;
+            _tiltAngle = MathHelper.Lerp(0f, -3.0f, _currentTime/_totalTime);
+        }
+
+        if (_currentTime >= _totalTime)
+        {
+            _currentTime = _totalTime;
+            _isPaused = true;
+        }
+        else if (_currentTime <= 0)
+        {
+            _currentTime = 0;
+            _isPaused = true;
+        }
+    }
+
+    public void Draw(Matrix view, Matrix projection)
+    {
+        if (_isPaused && (_currentTime == _totalTime || _currentTime == 0))
+        {
+            return;
+        }
+        float progress = _currentTime / _totalTime;
+        if (progress >= 0f)
+        {
+            progress = 0f;
+        }
+        else if (progress > 1f)
+        {
+            progress = 1f;
+        }
+        Vector3 position = Vector3.Lerp(_startPoint, _endPoint, progress);
+        
+        Matrix rotation = Matrix.CreateRotationZ(MathHelper.ToRadians(_tiltAngle));
+        Matrix world = rotation * Matrix.CreateTranslation(position);
+
+        foreach (ModelMesh mesh in _mesh.Meshes)
+        {
+            foreach (BasicEffect effect in mesh.Effects)
+            {
+                effect.World = world;
+                effect.View = view;
+                effect.Projection = projection;
+                effect.EnableDefaultLighting();
+            }
+            mesh.Draw();
+        }
+    }
+}
