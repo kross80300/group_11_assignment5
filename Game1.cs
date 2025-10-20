bd20fe3eb6d34d6a17279243ee8cafa11e6c0b57
anunayd15 <145937850+anunayd15@users.noreply.github.com>
2025-10-19 21:38:34 -0500

    Add files via upload

--- Game1.cs
+++ Game1.cs
@@ -1,78 +1,85 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
-namespace group_11_assignment5;
-
-public class Game1 : Game
+namespace project5
 {
-    private GraphicsDeviceManager _graphics;
-    private SpriteBatch _spriteBatch;
-    
-    
-    private Spaceship _spaceship;
-    private Model _spaceshipModel;
-
-    public Game1()
+    public class Game1 : Game
     {
-        _graphics = new GraphicsDeviceManager(this);
-        Content.RootDirectory = "Content";
-        IsMouseVisible = true;
-        Window.AllowUserResizing = true;
-    }
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
 
-    protected override void Initialize()
-    {
-        // TODO: Add your initialization logic here
-        
+        private Spaceship _spaceship;
+        private Model _spaceshipModel;
+        private List<Planet> _planets;
 
-        base.Initialize();
-    }
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            Window.AllowUserResizing = true;
+        }
 
-    protected override void LoadContent()
-    {
-        _spriteBatch = new SpriteBatch(GraphicsDevice);
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-        // TODO: use this.Content to load your game content here
-        _spaceshipModel = Content.Load<Model>("meshes/shuttle");
+            _spaceshipModel = Content.Load<Model>("meshes/shuttle");
 
-        Vector3 start = new Vector3(800f, 100f, 0f);
-        Vector3 end = new Vector3(-800f, 100f, 300f);
-        float travelTime = 8f;
-        float pauseTime = 4f;
-        
-        _spaceship = new Spaceship(_spaceshipModel, start, end, travelTime, pauseTime);
-    }
+            Vector3 start = new Vector3(800f, 100f, 0f);
+            Vector3 end = new Vector3(-800f, 100f, 300f);
+            float travelTime = 8f;
+            float pauseTime = 4f;
 
-    protected override void Update(GameTime gameTime)
-    {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
-            Keyboard.GetState().IsKeyDown(Keys.Escape))
-            Exit();
+            _spaceship = new Spaceship(_spaceshipModel, start, end, travelTime, pauseTime);
 
-        // TODO: Add your update logic here
-        _spaceship.Update(gameTime);
+            _planets = new List<Planet>();
 
-        base.Update(gameTime);
-    }
+            Model planet1Mesh = Content.Load<Model>("meshes/planet1");
+            Model planet2Mesh = Content.Load<Model>("meshes/planet2");
+            Model planet3Mesh = Content.Load<Model>("meshes/planet3");
 
-    protected override void Draw(GameTime gameTime)
-    {
-        GraphicsDevice.Clear(Color.Black);
+            _planets.Add(new Planet(planet1Mesh, Vector3.Zero, 200f, 1f, MathHelper.TwoPi / 6f));
+            _planets.Add(new Planet(planet2Mesh, Vector3.Zero, 350f, 0.8f, MathHelper.TwoPi / 12f));
+            _planets.Add(new Planet(planet3Mesh, Vector3.Zero, 500f, 0.6f, MathHelper.TwoPi / 18f));
+        }
 
-        // TODO: Add your drawing code here
-        Matrix view = Matrix.CreateLookAt(new Vector3(0, 300, 800),
-            Vector3.Zero,
-            Vector3.Up);
-        
-        Matrix projection = Matrix.CreatePerspectiveFieldOfView(
-            MathHelper.ToRadians(45f),
-            GraphicsDevice.Viewport.AspectRatio,
-            1f,
-            10000f);
-        
-        _spaceship.Draw(view, projection);
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
 
-        base.Draw(gameTime);
+            _spaceship.Update(gameTime);
+
+            foreach (var planet in _planets)
+                planet.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            Matrix view = Matrix.CreateLookAt(new Vector3(0, 300, 800),
+                Vector3.Zero,
+                Vector3.Up);
+
+            Matrix projection = Matrix.CreatePerspectiveFieldOfView(
+                MathHelper.ToRadians(45f),
+                GraphicsDevice.Viewport.AspectRatio,
+                1f,
+                10000f);
+
+            _spaceship.Draw(view, projection);
+
+            foreach (var planet in _planets)
+                planet.Draw(view, projection);
+
+            base.Draw(gameTime);
+        }
     }
 }
---
+++ Planet.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace project5
+{
+    public class Planet
+    {
+        private Model _mesh;
+        private Vector3 _center;
+        private Vector3 _position;
+        private float _orbitRadius;
+        private float _orbitSpeed;
+        private float _rotationSpeed;
+        private float _currentRotation;
+        private List<Moon> _moons;
+        public Planet(Model mesh, Vector3 center, float orbitRadius, float rotationSpeed, float orbitSpeed)
+        {
+            _mesh = mesh;
+            _center = center;
+            _orbitRadius = orbitRadius;
+            _rotationSpeed = rotationSpeed;
+            _orbitSpeed = orbitSpeed;
+            _position = center + new Vector3(orbitRadius, 0, 0);
+            _currentRotation = 0f;
+            _moons = new List<Moon>();
+        }
+        public Vector3 GetPosition()
+        {
+            return _position;
+        }
+        public void AddMoon(Moon moon)
+        {
+            if (moon != null)
+                _moons.Add(moon);
+        }
+        public void Update(GameTime gameTime)
+        {
+            float elapsedTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            float totalTime = (float)gameTime.TotalGameTime.TotalSeconds;
+            _currentRotation += _rotationSpeed * elapsedTime;
+            _currentRotation %= MathHelper.TwoPi;
+
+            float orbitPos = _orbitSpeed * totalTime;
+            Vector3 idealPos = _center + new Vector3(_orbitRadius * (float)Math.Cos(orbitPos), 0f,
+            _orbitRadius * (float)Math.Sin(orbitPos)
+            );
+            _position = Vector3.Lerp(_position, idealPos, 0.1f);
+
+            foreach (var moon in _moons)
+                moon.Update(gameTime);
+        }
+        public void Draw(Matrix view, Matrix projection)
+        {
+            Matrix world = Matrix.CreateRotationY(_currentRotation) *
+                           Matrix.CreateTranslation(_position);
+            foreach (var mesh in _mesh.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.World = world;
+                    effect.View = view;
+                    effect.Projection = projection;
+                    effect.EnableDefaultLighting();
+                }
+                mesh.Draw();
+            }
+            foreach (var moon in _moons)
+                moon.Draw(view, projection);
+        }
+    }
+}
